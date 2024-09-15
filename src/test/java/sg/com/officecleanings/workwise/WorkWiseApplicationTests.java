5335ca0e27e3241a281a1de91fb3899f9f39f464
Darryl Soh <ssy.darryl@gmail.com>
2024-09-15 20:30:44 +0800

    Initial commit

---
+++ src/test/java/sg/com/officecleanings/workwise/WorkWiseApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.com.officecleanings.workwise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkWiseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
